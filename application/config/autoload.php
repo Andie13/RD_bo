6344decd062f3daa19378b586c20d6a0bcd93337
Andie13 <fody.fady@gmail.com>
2019-12-09 14:33:13 +0100

    added model

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('Admin_model','Events_model', 'Users_model', 'Villes_model','Medias_model','Users_model','Presta_model','Statuts_model');
+$autoload['model'] = array('Admin_model','Events_model', 'Users_model', 'Villes_model','Medias_model','Users_model','Presta_model','Statuts_model','Resas_model');
