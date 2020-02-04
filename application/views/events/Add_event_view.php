a2925a7b0d4d20d78af1994ffa4f38d64c9f228e
Andie13 <fody.fady@gmail.com>
2020-02-04 15:28:03 +0100

    test

--- application/views/events/Add_event_view.php
+++ application/views/events/Add_event_view.php
@@ -221,24 +221,12 @@ showButtonPanel: true
 $.datepicker.setDefaults($.datepicker.regional['fr']);
 	    
 	    
-$('.timepicker').timepicker({
-    disableMousewheel: true,
-    icons: {
-        up: 'la la-angle-up',
-        down: 'la la-angle-down'
-    },
-    showSeconds: true,
-    showMeridian: false,
-    defaultTime: new Date()
-}).on('changeTime.timepicker', function (e) {
-
-    var hours = ('0' + e.time.hours).slice(-2);
-    var minutes = ('0' + e.time.minutes).slice(-2);
-    var seconds = ('0' + e.time.seconds).slice(-2);
-
-    $(this).val(hours + ':' + minutes + ':' + seconds);
+$(function () {
+    $('.timepicker').timepicker({
+        use24hours: true,
+        format: 'HH:mm'
+    });
 });
-
     });
 
 
