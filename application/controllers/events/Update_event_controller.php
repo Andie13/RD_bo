22a5cdb701346ec195315a858e7442845f01d7ac
Andie13 <fody.fady@gmail.com>
2020-03-10 17:56:10 +0100

    add 2 functions to events controller
    1 to lead to the new reservation screen
    2 to process the reservation

--- application/controllers/events/Update_event_controller.php
+++ application/controllers/events/Update_event_controller.php
@@ -124,5 +124,57 @@ class Update_event_controller extends CI_Controller {
            }
         }
     }
+	 public function toAddManualResa() {
+        $idEvent = $this->input->get('idEvent');
+
+        $eventModel = new Events_model();
+        $villeModel = new Villes_model();
+        $userModel = new Users_model();
+
+        $event = $eventModel->getEventDetailsById($idEvent);
+        $ville = $villeModel->getNomVilleFromId($event->id_ville);
+        $users = $userModel->getAllUsers();
+        $promos = $eventModel->getAllFromPromo();
+        $typePayment = $eventModel->getAllFromTypePaymt();
+
+        $data['event'] = $event;
+        $data['ville'] = $ville;
+        $data['users'] = $users;
+        $data['pomo'] = $promos;
+        $data['typePayment'] = $typePayment;
+
+
+        $this->load->view('layout/header');
+        $this->load->view('layout/sidebar');
+
+        $this->load->view('events/add_resa_to_event', $data);
+    }
+
+    public function addResa() {
+
+        //récupération des valeur du post
+        $idEvent = $this->input->post('eventId');
+        $userId = $this->input->post('userId');
+        $tpePay = $this->input->post('typePay');
+        $promo = $this->input->post('promo');
+        $prix = $this->input->post('prix');
+
+        $_price = $prix - $prix*$promo;
+        //insertion de la résa en base
+        $eventModel = new Events_model();
+        $res = $eventModel->insertNewReservation($userId, $idEvent,$tpePay,$promo,$_price);
+
+        
+        //if reservation was correctly inserted
+        if ($res != FALSE) {
+
+            $this->session->set_flashdata('success', "La réservation a été enregistrée avec succès.");
+            redirect("Events_controller/displayEventDetails?id=$idEvent");
+        } else {
+            $this->session->set_flashdata('err', "Nous n'avons pu mettre inserrer cette réservation.");
+            redirect("Events_controller/displayEventDetails?id=$idEvent");
+        }
+    }
+
 
 }
