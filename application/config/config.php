4ae246aff75c215d1ecf0c5f8d593df8bf7eaeaf
Andie13 <fody.fady@gmail.com>
2020-03-11 15:00:40 +0100

    test

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://Realdating.bo';
+$config['base_url'] = 'http://back.dev.Realdating.bo';
 /*
 |--------------------------------------------------------------------------
 | Index File
